18c82f4f483836b0c508828965a278c8fe138c7e
Dilawar Singh <dilawars@ncbs.res.in>
2015-03-25 14:59:07 +0530

    Adding a parser to parse the rule file.

---
+++ main.py
@@ -0,0 +1,10 @@
+import parser 
+
+def main(ruleFile):
+    parser.parse_rulefile(ruleFile)
+    print parser.rules
+
+
+if __name__ == '__main__':
+    ruleFile = sys.argv[1]
+    main(ruleFile)
---
+++ parser.py
@@ -0,0 +1,9 @@
+import re
+import sys
+import os
+
+rules = {}
+
+def parse_rulefile(rules):
+    '''Parse the rule file'''
+    print rules
