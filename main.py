cfbea97eba9b834365ea61708cf9e63b7107ff1a
Dilawar Singh <dilawars@ncbs.res.in>
2015-03-25 15:08:02 +0530

    Primitive parser is done. It stores all the rules in parser.rules dictionary.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import parser 
 
 def main(ruleFile):
--- parser.py
+++ parser.py
@@ -2,8 +2,16 @@ import re
 import sys
 import os
 
+# This dictionary keeps all the rules from rules.txt file. Key is condition, and
+# value associated with key is action.
 rules = {}
 
-def parse_rulefile(rules):
+def parse_rulefile(rule_file):
     '''Parse the rule file'''
-    print rules
+    print("Parsing rule file: %s" % rule_file)
+    with open(rule_file, "r") as rF:
+        for line in rF:
+            line = line.strip()
+            if not line: continue
+            condition, action = line.split("->")
+            rules[condition] = action
