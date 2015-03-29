63ab16199248b7ac70625d9025efed3bdef01fe5
Dilawar Singh <dilawars@ncbs.res.in>
2015-03-29 17:06:20 +0530

    Test is complete.

--- glycon.py
+++ glycon.py
@@ -25,6 +25,7 @@ class Glycon():
         self.shapes = { 'C' : 'circle'
                 , 'R' : 'rect'
                 , 'T' : 'triangle' }
+        self.colors = { 0 : "red", 1 : "blue", 2 : "yellow" }
         self.addNode(root)
 
     def num_monomers(self):
@@ -72,6 +73,7 @@ class Glycon():
             , label = monomer.__repr__()
             , shape=self.shapes[monomer.shape]
             , carbon_index=monomer.carbonIndex
+            , color=self.colors[monomer.carbonIndex]
             )
 
 
@@ -88,10 +90,13 @@ class Glycon():
 if __name__ == "__main__":
 
     m1, m2, m3 = Monomer(1, "C"), Monomer(0, "R"), Monomer(1, "T")
+    m4, m5, m6 = Monomer(1, "C"), Monomer(0, "R"), Monomer(1, "T")
     glycan = Glycon("G1", m1)
-    glycan.addMonomer(m2)
-    glycan.addMonomer(m2)
+    glycan.addMonomer(m2, m1)
     glycan.addMonomer(m3, m2)
+    glycan.addMonomer(m4, m1)
+    glycan.addMonomer(m5, m4)
+    glycan.addMonomer(m6, m5)
     #glycan.writeGraphviz()
     nx.write_dot(glycan.topology, "network.dot")
     #nx.draw(glycan.topology)
